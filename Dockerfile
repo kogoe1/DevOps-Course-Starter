7d21f4a1452d51c50eeab301b36bd36581698ede
kogoe1 <fiifiogoe@yahoo.com>
2021-05-03 14:59:51 +0100

    Added create_app back into app.py, modified Dockerfile to remov virtual environment ENV config, added .venv to dockerignore file and removed duplicate pytest line from project.toml file

--- Dockerfile
+++ Dockerfile
@@ -6,12 +6,11 @@ ENV PATH="${PATH}:/root/.poetry/bin"
 RUN mkdir -p /devops_mod5
 WORKDIR /devops_mod5
 COPY . /devops_mod5
-RUN poetry install 
-ENV VIRTUAL_ENV=.venv
-ENV PATH="$VIRTUAL_ENV/bin:$PATH"
 
 FROM base AS production
-ENTRYPOINT ["gunicorn", "-w", "3", "-b", "0.0.0.0:5000", "todo_app.app:app"]
+RUN poetry config virtualenvs.create false --local && poetry install --no-dev --no-root
+ENTRYPOINT ["gunicorn", "-w", "3", "-b", "0.0.0.0:5000", "todo_app.app:create_app()"]
 
 FROM base AS development
+RUN poetry install 
 ENTRYPOINT ["poetry", "run", "flask", "run", "--host", "0.0.0.0"] 
--- todo_app/app.py
+++ todo_app/app.py
@@ -10,67 +10,69 @@ from dotenv import load_dotenv, find_dotenv
 
 import os
 
+def create_app():
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    file_path = find_dotenv('.env')
+    load_dotenv(file_path, override=True)
 
-file_path = find_dotenv('.env')
-load_dotenv(file_path, override=True)
-
-app.config.from_object('todo_app.flask_config.Config')
-    
-
-TRELLO_API_KEY=os.environ.get('TRELLO_API_KEY')
-TRELLO_TOKEN=os.environ.get('TRELLO_TOKEN')    
-BOARD_ID=os.environ.get('BOARD_ID')  
-    
-trello_util = TrelloUtility(TRELLO_API_KEY, TRELLO_TOKEN, BOARD_ID)
-
-def taskSort(item):
-        return item.status
-
-@app.route('/', methods=['GET'])
-def index():
-    form = TodoForm()
-    items = trello_util.get_items()
-    items.sort(reverse=True, key=taskSort)  
+    app.config.from_object('todo_app.flask_config.Config')
         
-    item_view_model = ViewModel(items)
-    return render_template('index.html', view_model=item_view_model, form=form)
 
-@app.route('/', methods=['POST'])
-def index_form():
-    form = TodoForm()
-    if form.validate_on_submit():
-        title = request.form['title']
-        description = request.form['description']
-        trello_util.add_item(title, description)
+    TRELLO_API_KEY=os.environ.get('TRELLO_API_KEY')
+    TRELLO_TOKEN=os.environ.get('TRELLO_TOKEN')    
+    BOARD_ID=os.environ.get('BOARD_ID')  
+        
+    trello_util = TrelloUtility(TRELLO_API_KEY, TRELLO_TOKEN, BOARD_ID)
 
-    return redirect('/')            
+    def taskSort(item):
+            return item.status
 
-@app.route('/in_progress/<id>', methods=['GET'])
-def in_progress(id):
-    trello_util.update_item(id, trello_util.STATUS_IN_PROGRESS)
+    @app.route('/', methods=['GET'])
+    def index():
+        form = TodoForm()
+        items = trello_util.get_items()
+        items.sort(reverse=True, key=taskSort)  
+            
+        item_view_model = ViewModel(items)
+        return render_template('index.html', view_model=item_view_model, form=form)
 
-    return redirect ('/')
+    @app.route('/', methods=['POST'])
+    def index_form():
+        form = TodoForm()
+        if form.validate_on_submit():
+            title = request.form['title']
+            description = request.form['description']
+            trello_util.add_item(title, description)
 
-@app.route('/complete/<id>', methods=['GET'])
-def completed(id):
-    trello_util.update_item(id, trello_util.STATUS_COMPLETED)
+        return redirect('/')            
 
-    return redirect ('/')
+    @app.route('/in_progress/<id>', methods=['GET'])
+    def in_progress(id):
+        trello_util.update_item(id, trello_util.STATUS_IN_PROGRESS)
 
-@app.route('/not_started/<id>', methods=['GET'])
-def not_started(id):
-    trello_util.update_item(id, trello_util.STATUS_NOT_STARTED)
+        return redirect ('/')
 
-    return redirect ('/')
+    @app.route('/complete/<id>', methods=['GET'])
+    def completed(id):
+        trello_util.update_item(id, trello_util.STATUS_COMPLETED)
 
-@app.route('/remove/<id>', methods=['GET'])
-def remove_task(id):
-    trello_util.delete_item(id)
+        return redirect ('/')
 
-    return redirect ('/')
+    @app.route('/not_started/<id>', methods=['GET'])
+    def not_started(id):
+        trello_util.update_item(id, trello_util.STATUS_NOT_STARTED)
+
+        return redirect ('/')
+
+    @app.route('/remove/<id>', methods=['GET'])
+    def remove_task(id):
+        trello_util.delete_item(id)
+
+        return redirect ('/')
 
 
-if __name__ == '__main__':
-    app.run()
+    if __name__ == '__main__':
+        app.run()
+
+    return app
